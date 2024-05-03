eff62f8b7f4f31a588711837a3b0b3c3eaca8b14
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-05-03 11:58:09 +0800

    updated composables

--- app/src/main/java/com/example/anicompose/MainActivity.kt
+++ app/src/main/java/com/example/anicompose/MainActivity.kt
@@ -5,20 +5,38 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.ui.unit.dp
 import com.example.anicompose.ui.theme.AniComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+//        installSplashScreen()
         setContent {
             AniComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -41,10 +59,56 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+//@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     AniComposeTheme {
         Greeting("Android")
     }
 }
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun NavigationBarSample() {
+    data class NavigationItem(val title: String, val icon: ImageVector)
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf(
+        NavigationItem("Anime", Icons.Filled.Favorite),
+        NavigationItem("Manga", Icons.Filled.AccountBox),
+        NavigationItem("Community", Icons.Filled.AccountCircle),
+        NavigationItem("About", Icons.Filled.Info),
+    )
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun HeroBox() {
+    Card (
+        shape = RoundedCornerShape(15.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.DarkGray, //Card background color
+            contentColor = Color.White  //Card content color,e.g.text
+        ),
+        modifier = Modifier.size(width = 400.dp,height = 200.dp,)
+    ){
+        Box {
+            Text(text = "test");
+        }
+    }
+}
+
+
+
+
+
